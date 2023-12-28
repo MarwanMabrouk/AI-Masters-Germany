dbf70a579bb15cd3a0723ea5ae6b3c81cf01df4d
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2023-12-28 22:35:58 +0100

    Plot result of course clustering

---
+++ AI_masters_germany/plotting.py
@@ -0,0 +1,24 @@
+import plotly.express as px
+
+
+def plot_clusters(clustering_result, show_plot=True):
+    """
+    Plot course clustering result.
+
+    :param clustering_result: Result of cluster_courses method.
+    :param show_plot: Set to True to show the plot.
+    :return: Plotly figure object.
+    """
+    fig = px.scatter(
+        clustering_result,
+        x="Component_1",
+        y="Component_2",
+        color='Cluster',
+        title='Course clustering result',
+        hover_data=['Course Name']
+    )
+
+    if show_plot:
+        fig.show()
+
+    return fig
--- tests/clustering.py
+++ tests/clustering.py
@@ -1,8 +1,10 @@
 import pandas as pd
-from AI_masters_germany import clustering, plotting
+from AI_masters_germany import utils, clustering, plotting
 
 if __name__ == '__main__':
     df = pd.read_csv('../dataset.csv')
+    df = utils.database_preprocessing(df=df, remove_stopwords=True)
     clustering_result = clustering.cluster_courses(
         df=df
     )
+    plotting.plot_clusters(clustering_result)
