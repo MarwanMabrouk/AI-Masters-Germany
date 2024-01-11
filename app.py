ef79fcff14f681e040f266fa1ae744079bd54fee
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2024-01-11 22:34:38 +0100

    Add some interactive elements to course clustering plot

--- app.py
+++ app.py
@@ -1,35 +1,70 @@
 import pandas as pd
 from AI_masters_germany import utils, clustering, plotting
+from AI_masters_germany.aim import AIM
 from flask import Flask,render_template, request
 import numpy as np
 import json
 import plotly
+import threading
 
 app = Flask(__name__)
+aim = AIM()  # Object that allows clustering etc.
 
-database = pd.read_csv("dataset.csv")
-database = utils.database_preprocessing(database, remove_stopwords=False)
-database_without_stopwords = utils.database_preprocessing(database, remove_stopwords=True)
-
-
-# We need already do the clustering
-# Doing this at the time the user wants to see the clustering site takes to long!
-clustering_result = clustering.cluster_courses(
-    df=database_without_stopwords,
-    k_ranges=np.linspace(80, 111, 30)
-)
 
 @app.route("/")
 def firstPage():
     return render_template("firstPage.html")
 
-@app.route("/course_clustering")
+
+@app.route("/course_clustering", methods=['GET', 'POST'])
 def course_clustering():
-    fig = plotting.plot_clusters(clustering_result, show_plot=False)
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template("course_clustering.html", graphJSON=graphJSON)
+
+    database = aim.get_database()
+    clusters = aim.get_clustered_courses()
+
+    if request.method == "POST":
+        min_credits = int(request.form["min_credits"])
+        max_credits = int(request.form["max_credits"])
+
+        credits_condition = (
+            (database["ECTS"] >= min_credits) &
+            (database["ECTS"] <= max_credits)
+        )
+
+        lecture_type = request.form["lecture_type"]
+        if lecture_type == 'all':
+            lecture_type_condition = pd.Series([True] * len(database["Type"]))
+        elif lecture_type == 'Obligatory':
+            lecture_type_condition = database["Type"] == "Obligatory"
+        else:
+            lecture_type_condition = database["Type"] == "Elective"
+
+        data = clusters[credits_condition & lecture_type_condition]
+
+    else:
+        min_credits = 1
+        max_credits = int(database["ECTS"].max())
+
+        lecture_type = 'all'
+
+        data = clusters
+
+    fig = plotting.plot_clusters(data, show_plot=False)
+    fig_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template("course_clustering.html",
+                           fig_json=fig_json,
+                           min_credits=min_credits, max_credits=max_credits,
+                           lecture_type=lecture_type)
 
 
 if __name__ == '__main__':
+    load_database_thread = threading.Thread(target=aim.load_database, args=('./dataset.csv',))
+    course_clustering_thread = threading.Thread(target=aim.cluster_courses, args=())
+
+    load_database_thread.start()
+    load_database_thread.join()
+    course_clustering_thread.start()
+
     app.debug = True
     app.run()
