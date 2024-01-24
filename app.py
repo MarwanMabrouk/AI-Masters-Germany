b38f5214573f9aa475724241342949552a908b09
Ula Muhabbek <153080098+Ula-MK@users.noreply.github.com>
2024-01-24 16:30:08 +0100

    Add changes

--- app.py
+++ app.py
@@ -10,12 +10,29 @@ import threading
 app = Flask(__name__)
 aim = AIM()  # Object that allows clustering etc.
 
+def get_data():
+    data=pd.read_excel("University Data LA Project(4).xlsx")
+    course_name=data['Course Name'].tolist()
+    degree_Name=data['Degree Name'].unique().tolist()
+    uni_fachhochschule_tu=data['Uni/Fachhochschule/TU'].unique().tolist()
+    #uni_name=data['Uni Name'].unique().tolist()
+    ccn=len(course_name)
+    cdn=len(degree_Name)
+    cuft=len(uni_fachhochschule_tu)
+    return course_name, degree_Name,uni_fachhochschule_tu,ccn,cdn,cuft
 
 @app.route("/")
 def firstPage():
-    return render_template("firstPage.html")
+    course_name, degree_Name, uni_fachhochschule_tu, ccn, cdn, cuft = get_data()
+    return render_template("firstPage.html", course_name=course_name, degree_Name=degree_Name,
+                           uft=uni_fachhochschule_tu, ccn=ccn, cdn=cdn, cuft=cuft)
 
 
+@app.route("/search")
+def second_page():
+    course_name, degree_Name, uni_fachhochschule_tu, ccn, cdn, cuft = get_data()
+    return render_template("second_page.html", course_name=course_name, degree_Name=degree_Name,
+                           uft=uni_fachhochschule_tu, ccn=ccn, cdn=cdn, cuft=cuft)
 @app.route("/institute_types")
 def institute_types():
 
