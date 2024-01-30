5dc692a65250b9b8b628ce8e45b2d96e945adc3d
Marawan Abdelgawad <marawan.abdelgawad@stud.uni-due.de>
2024-01-30 14:57:21 +0100

    Added Database config params

--- app.py
+++ app.py
@@ -10,18 +10,10 @@ import json
 import plotly
 import threading
 from pymongo import MongoClient
-
+from config import CONNECTION_STRING
 #TODO: convert credentials to environment variables
 print('Connecting to database...')
-USERNAME = "doadmin"
-PASSWORD = "Rj5M6X7e219DvQ43"
-DB_NAME = "data"
-CONNECTION_STRING = 'mongodb+srv://{username}:{password}@{host}/{dbname}?{options}'.\
-    format(username=USERNAME,
-           password=PASSWORD,
-           host='ai-masters-8e63c445.mongo.ondigitalocean.com',
-           dbname=DB_NAME,
-           options='tls=true&authSource=admin&replicaSet=ai-masters')
+
 client = MongoClient(CONNECTION_STRING)
 
 db = client['data']
