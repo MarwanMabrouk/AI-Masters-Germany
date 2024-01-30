3780bc86b433bbe6340b65be4afc1f0c5188aa17
Marawan Abdelgawad <marawan.abdelgawad@stud.uni-due.de>
2024-01-30 14:57:21 +0100

    Added Database config params

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
-DATABASE_NAME = "test"
-COLLECTION_NAME = "test"
+USERNAME = "doadmin"
+PASSWORD = "Rj5M6X7e219DvQ43"
+DB_NAME = "data"
+CONNECTION_STRING = 'mongodb+srv://{username}:{password}@{host}/{dbname}?{options}'.\
+    format(username=USERNAME,
+           password=PASSWORD,
+           host='ai-masters-8e63c445.mongo.ondigitalocean.com',
+           dbname=DB_NAME,
+           options='tls=true&authSource=admin&replicaSet=ai-masters')
